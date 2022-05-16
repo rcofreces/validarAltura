6f0c0078b235a44d15d0d8915b3c1fb977adda16
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 14:15:52 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let altura: number = Number(prompt("Ingrese su altura"));
+
+if (altura >= 1.3) {
+  console.log("Usted puede entrar a la montaña rusa");
+} else {
+  console.log("Usted no tiene la altura permitida");
+}
